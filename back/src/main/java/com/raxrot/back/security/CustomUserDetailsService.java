922c10431d0556e4885524656ea1bc34c1aceac7
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-06-26 23:04:52 +0100

    add CustomUserDetailsService

---
+++ back/src/main/java/com/raxrot/back/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.raxrot.back.security;
+import com.raxrot.back.entity.User;
+import com.raxrot.back.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(()->new UsernameNotFoundException(username));
+        return AuthUser.builder().user(user).build();
+    }
+}
