76f0d4c27aa5880f20ddf528411bbbaaaf872ad7
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-06-28 18:07:04 +0100

    add TaskResponse

---
+++ back/src/main/java/com/raxrot/back/dto/TaskResponse.java
@@ -0,0 +1,24 @@
+package com.raxrot.back.dto;
+
+import com.raxrot.back.enums.Priority;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TaskResponse {
+    private Long id;
+    private String title;
+    private String description;
+    private Boolean completed;
+    private Priority priority;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime dueDate;
+}
